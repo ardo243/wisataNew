e18973509d9dc7e0584dfc6961d0fd9ed5ee296e
ardo <zoliardo@students.amikom.ac.id>
2025-12-23 12:01:52 +0700

    update

--- app/src/main/java/com/example/diswis/ActivityHome.kt
+++ app/src/main/java/com/example/diswis/ActivityHome.kt
@@ -1,11 +1,13 @@
 package com.example.diswis
 
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.LinearLayout
 import android.content.Intent
 
 class ActivityHome : AppCompatActivity() {
@@ -14,16 +16,43 @@ class ActivityHome : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
 
-        val menuDestinasi = findViewById<LinearLayout>(R.id.menu_Distinasi)
-        menuDestinasi.setOnClickListener {
-            val intent = Intent(this,menuDestinasi::class.java)
-            startActivity(intent)
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Setup RecyclerView
+        val rvDestinations = findViewById<RecyclerView>(R.id.rv_destinations)
+        rvDestinations.layoutManager = LinearLayoutManager(this)
+        rvDestinations.isNestedScrollingEnabled = false
+        val destinations = listOf(
+            Destination("Candi Prambanan", "Rp 50.000", R.drawable.candi_prambanan),
+            Destination("Candi Prambanan", "Rp 50.000", R.drawable.candi_prambanan),
+            Destination("Tempat Lain", "Rp 75.000", R.drawable.jogja)
+        )
+        val adapter = DestinationAdapter(destinations)
+        rvDestinations.adapter = adapter
+
+        // Setup Category Click (Placeholder)
+        val menuDestinasi = findViewById<LinearLayout>(R.id.menu_Distinasi)
+        menuDestinasi.setOnClickListener {
+            // TODO: Navigate to Destinasi Page
+            // val intent = Intent(this, DestinationActivity::class.java)
+            // startActivity(intent)
+        }
+
+        // Navigation to Profile
+        val profileImage = findViewById<android.widget.ImageView>(R.id.profile_image)
+        profileImage.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        val navUser = findViewById<android.widget.ImageView>(R.id.nav_user)
+        navUser.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/diswis/MainActivity.kt
+++ app/src/main/java/com/example/diswis/MainActivity.kt
@@ -10,7 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+        val btnMasuk = findViewById<android.widget.Button>(R.id.btnMasuk)
+        val btnDaftar = findViewById<android.widget.Button>(R.id.btnDaftar)
+        btnMasuk.setOnClickListener {
+            val intent = android.content.Intent(this, ActivityLogin::class.java)
+            startActivity(intent)
+        }
+        btnDaftar.setOnClickListener {
+            val intent = android.content.Intent(this, ActivityRegestrasi::class.java)
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
